ee4710f5a126ce369ef954a677924e28b4524e8d
aplneto <apln2@cin.ufpe.br>
2019-12-17 19:04:31 -0300

    extension communicating trough stdout

--- src/main/java/br/com/tempest/JSReader.java
+++ src/main/java/br/com/tempest/JSReader.java
@@ -1,9 +1,37 @@
 package br.com.tempest;
 
+import java.util.regex.*;
+import java.util.LinkedList;
+import java.util.List;
+
 public class JSReader
 {
-    public static String helloWorld()
+    // Matches generic URI patterns
+    public static Pattern uriPattern = Pattern.compile("\\w+:(//)?(\\w*[\\./]?)+");
+    // Matches common string association patterns in JavaScript
+    public static Pattern stringAssociationPattern = Pattern.compile(
+        "((var|let|const)\\s)?\\w+\\s?\\+?=\\s?(('([^'])*')|(\"[^\"]*\"))"
+    );
+    // Matches common string patterns
+    public static Pattern stringPattern = Pattern.compile("(('([^'])*')|(\"[^\"]*\"))");
+    // Matches generic local path strings
+    public static Pattern localPathPattern = Pattern.compile(
+        "['\"](\\w+[\\\\/]?)+['\"]"
+    );
+    // Matches
+    public static List<String> lookForMatches(String script, Pattern searchPattern)
     {
-        return "Olá Mundo!";
+        Matcher matcher = searchPattern.matcher(script);
+        LinkedList<String> results = new LinkedList<String>();
+        while (matcher.find())
+        {
+            results.add(script.substring(matcher.start(), matcher.end()));
+        }
+        return results;
+    }
+
+    public static List<String> parse(String script)
+    {
+        return null;
     }
 }
--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,34 +1,62 @@
 package burp;
 
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
+// import br.com.tempest.JSReader;
 
-public class BurpExtender implements IBurpExtender, IScannerCheck
+public class BurpExtender implements IBurpExtender, IHttpListener//, IScannerCheck
 {
-    IBurpExtenderCallbacks callbacks;
-    IExtensionHelpers helpers;
+    private IBurpExtenderCallbacks callbacks;
+    private IExtensionHelpers helpers;
+    PrintWriter stdout;
 
+    @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
     {
         this.callbacks = callbacks;
-        this.helpers = callbacks.getHelpers();
 
         this.callbacks.setExtensionName("JavaScript Reader");
-    }
-    
-    public int consolidateDuplicateIssues(IScanIssue existingIssue, IScanIssue newIssue){
-        return 0;
-    }
-    
+        this.callbacks.registerHttpListener(this);
+        // this.callbacks.registerScannerCheck(this);
 
-    public List<IScanIssue> doActiveScan(IHttpRequestResponse basRequestResponse,
-                                        IScannerInsertionPoint insertionPoint)
-    {
-        return null;
+        this.helpers = callbacks.getHelpers();
+
+        stdout = new PrintWriter(this.callbacks.getStdout(), true);
     }
 
-    public List<IScanIssue> doPassiveScan(IHttpRequestResponse baseRequestResponse)
+    @Override
+    public void processHttpMessage( int toolFlag,
+    boolean messageIsRequest, IHttpRequestResponse messageInfo)
     {
-        return null;
+        if (!messageIsRequest)
+        {
+            byte[] response = messageInfo.getResponse();
+            IResponseInfo responseInfo = helpers.analyzeResponse(response);
+            int bodyOffset = responseInfo.getBodyOffset();
+            byte[] responseBody = Arrays.copyOfRange(response, bodyOffset, response.length);
+            String body = new String(responseBody);
+            stdout.println(body);
+        }
     }
+    
+    // public int consolidateDuplicateIssues(IScanIssue existingIssue, IScanIssue newIssue){
+    //     return 0;
+    // }
+    
+
+    // public List<IScanIssue> doActiveScan(IHttpRequestResponse basRequestResponse,
+    //                                     IScannerInsertionPoint insertionPoint)
+    // {
+    //     return null;
+    // }
+
+    // public List<IScanIssue> doPassiveScan(IHttpRequestResponse baseRequestResponse)
+    // {
+    //     String response = baseRequestResponse.getResponse().toString();
+    //     List<String> matches;
+    //     matches = JSReader.lookForMatches(response, JSReader.uriPattern);
+    //     return null;
+    // }
     
 }
